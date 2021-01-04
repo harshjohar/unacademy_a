c409ab19d621b6a6f03dc88c3aeac0ef758b954b
Harshpreet Singh Johar <50266759+harshjohar@users.noreply.github.com>
2021-01-04 23:25:54 +0530

    fianlly done

--- assignment_3/q9.py
+++ assignment_3/q9.py
@@ -1,6 +1,18 @@
 n = int(input())
 
-for i in range (n):
-    print(i)
+gcd = 1
 
-# todo
+a = int(input())
+b = int(input())
+
+for i in range(1, max(a, b)+1):
+    if a%i == 0 and b%i == 0:
+        gcd_o = i
+
+for k in range(1, n-1):
+    c = int(input())
+    for j in range(1, max(gcd_o, c)+1):
+        if c%j == 0 and gcd_o%j == 0:
+            gcd = j
+
+print('gcd is: '+ str(gcd))
