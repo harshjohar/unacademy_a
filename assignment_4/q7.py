5c85da69f4c7d920cbcaba81a76a1d4f67758c39
Harshpreet Singh Johar <50266759+harshjohar@users.noreply.github.com>
2021-01-10 15:45:52 +0530

    correction

--- assignment_4/q7.py
+++ assignment_4/q7.py
@@ -3,6 +3,8 @@ arr = []
 
 for i in range(d):
     c = int(input())
+    if c == 0:
+        c = 1
     arr.append(c)
 
 count = 0
@@ -11,4 +13,14 @@ for j in range(d-1):
     if arr[j] * arr[j+1] < 0:
         count += 1
 
-print(count)
+output = []
+
+if count >= 2:
+    while count >= 0:
+        output.append(count)
+        count -= 2
+    for i in range(len(output)):
+        print(output[len(output) - i - 1], end=' ')
+    print('')
+else:
+    print(count)
