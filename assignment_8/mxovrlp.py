378ad1f7130113587aaf82d589fcf27cd06ddb40
Harshpreet Singh Johar <50266759+harshjohar@users.noreply.github.com>
2021-02-23 00:30:24 +0530

    repaired

--- assignment_8/mxovrlp.py
+++ assignment_8/mxovrlp.py
@@ -9,17 +9,16 @@ for _ in range(t):
 		if s in ongoing_classes:
 			ongoing_classes[s]+=1
 		else:
-			ongoing_classes[s] = 1
+			ongoing_classes[s]=1
 			times.append(s)
-		
 		if e+1 in ongoing_classes:
-			ongoing_classes[e+1] = ongoing_classes[e+1]-1
+			ongoing_classes[e+1]-=1
 		else:
 			times.append(e+1)
-			ongoing_classes[e+1] = -1
+			ongoing_classes[e+1]=-1
+	times.sort()
 	result = 0
 	sum_class = 0
-	times.sort()
 	for i in times:
 		sum_class += ongoing_classes[i]
 		result = max(result, sum_class)
