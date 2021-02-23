bab27ed76e0664e04fb56767a72ff2a307f9e8f6
Harshpreet Singh Johar <50266759+harshjohar@users.noreply.github.com>
2021-02-24 00:17:30 +0530

    one left

--- assignment_9/adjsrt.py
+++ assignment_9/adjsrt.py
@@ -2,3 +2,20 @@ n = int(input())
 li = list(map(int, input().split()))
 
 # maybe bubble sort
+def adjacent_sort(li):
+    ans = []
+    for j in range(len(li)):
+        swap = False
+        for i in range(len(li)-j-1):    # did this because the comparison with last elemnt is not required
+            if li[i] > li[i+1]:
+                li[i], li[i+1] = li[i+1], li[i]
+                ans.append(i)
+                swap = True
+        if not swap:
+            return ans # if no swap in whole iteration menas the list is sorted
+    return ans
+
+ans = adjacent_sort(li)
+print(len(ans))
+for i in range(len(ans)):
+    print(ans[i], end=' ')
--- assignment_9/srtprb2.py
+++ assignment_9/srtprb2.py
@@ -1,4 +1,22 @@
 n = int(input())
 teams = list(map(int, input().split()))
-profitable_matches = 0
+profitable_matches = [0]
 # recursion may help here
+# hint: merge sort
+# if(len(set(Alist)) == len(Alist)): -> to check if the list contains all the unique element:
+
+def team_fixtures(li, left, right, result):
+    if len(li)<=1:
+        return
+    mid = (left+right)//2
+    l_h = li[left:mid+1]
+    r_h = li[mid+1:right+1]
+
+    if len(set(l_h)&set(r_h)) == 0:
+        result[0]+=1
+    team_fixtures(l_h, left, mid, result)
+    team_fixtures(r_h, mid+1, right, result)
+
+team_fixtures(teams, 0, len(teams)-1, profitable_matches)
+print(profitable_matches[0])
+
